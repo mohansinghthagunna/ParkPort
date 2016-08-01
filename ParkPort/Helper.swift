d94f8ad9101051a21972509f5732d8235a9ef3a3
Mohan Singh Thagunna <Mohan@sujits-MacBook-Pro.local>
2016-08-01 07:31:42 +0545

    start up

---
+++ ParkPort/Helper.swift
@@ -0,0 +1,15 @@
+//
+//  Helper.swift
+//  Waste Info Swift
+//
+//  Created by Creatu Developers on 6/9/16.
+//  Copyright © 2016 Mohan. All rights reserved.
+//
+
+import UIKit
+
+let AppName:String = "ParkPort"
+
+class Helper: NSObject {
+
+}
